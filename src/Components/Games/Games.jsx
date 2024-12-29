3a2552ecf3b2341bc7a1ee4edd30b788c74068b2
Shubhranil-Basak <Shubhranil.Basak@iiitb.ac.in>
2024-12-30 03:08:59 +0530

    games page handler

--- src/Components/Games/Games.jsx
+++ src/Components/Games/Games.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from "react";
+import GamesCard from "./GameCard";
+import { Container, Row, Col } from "react-bootstrap";
+import coin from "../../assets/coin.png";
 
 const Games = () => {
-    return(
-        <h1>Games section</h1>
-    );
-
+  return (
+    <>
+      <Container fluid className="game-section">
+        <Container>
+          <h1 className="game-heading">
+            <strong>Games</strong>
+          </h1>
+          <Row
+            className="justify-content-center"
+            style={{ paddingBottom: "10px" }}
+          >
+            <Col xs={12} sm={6} md={4} lg={3} className="game-card">
+              <GamesCard imgPath={coin} title="Coin Flip" />
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+    </>
+  );
 };
 
-export default Games
+export default Games;
