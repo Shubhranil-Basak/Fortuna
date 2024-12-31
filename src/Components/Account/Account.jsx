193c44bd88ea33d7c94ee73bf2818b209fed83c8
Shubhranil-Basak <Shubhranil.Basak@iiitb.ac.in>
2025-01-01 04:10:32 +0530

    customised the account section

--- src/Components/Account/Account.jsx
+++ src/Components/Account/Account.jsx
@@ -1,9 +1,47 @@
 import React from "react";
+import { useAuth } from "../../AuthContext";
+import { useState, useEffect } from "react";
+import { db } from "../../config/firebase";
+import { doc, getDoc } from "firebase/firestore";
 
 const Account = () => {
-    return(
-        <h1>Account section</h1>
-    );
+  const { user } = useAuth();
+  const [balance, setBalance] = useState(0);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      if (user) {
+        const userDoc = doc(db, "users", user.uid);
+        const docSnapshot = await getDoc(userDoc);
+
+        if (docSnapshot.exists()) {
+          setBalance(docSnapshot.data().balance);
+        } else {
+          console.log("No user data found!");
+        }
+      }
+    };
+
+    fetchUserData();
+  }, [user]);
+
+  return (
+    <div className="account-container" style={{color: "white"}}>
+      <h2>Account Details</h2>
+      {user ? (
+        <div>
+          <p>
+            <strong>Email:</strong> {user.email}
+          </p>
+          <p>
+            <strong>Balance:</strong> {balance} Coins
+          </p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 };
 
 export default Account;
