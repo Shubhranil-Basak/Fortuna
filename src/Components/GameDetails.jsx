8e9d98e1337375425e0ec52dcdbc2569003e732a
Shubhranil-Basak <Shubhranil.Basak@iiitb.ac.in>
2025-01-03 16:02:17 +0530

    added 3-cards to game route

--- src/Components/GameDetails.jsx
+++ src/Components/GameDetails.jsx
@@ -4,6 +4,7 @@ import CoinFlip from "./Gambles/CoinFlip";
 import Wheel_ from "./Gambles/Wheel";
 import RPS from "./Gambles/RPS";
 import _7UD from "./Gambles/7UD";
+import _3Cards from "./Gambles/3Cards";
 
 const GameDetails = () => {
   const { gameName } = useParams();
@@ -17,6 +18,7 @@ const GameDetails = () => {
       {gameName === "wheel-of-fortune" && <Wheel_ />}
       {gameName === "rock-paper-scissors" && <RPS />}
       {gameName === "7-up-7-down" && <_7UD />}
+      {gameName === "3-cards" && <_3Cards />}
     </div>
   );
 };
