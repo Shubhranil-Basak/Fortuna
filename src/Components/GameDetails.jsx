474821c35093fb2cf4f8362d79c246c0debac5ff
Shubhranil-Basak <Shubhranil.Basak@iiitb.ac.in>
2025-01-05 04:34:13 +0530

    Added HiLo to games route

--- src/Components/GameDetails.jsx
+++ src/Components/GameDetails.jsx
@@ -5,6 +5,7 @@ import Wheel_ from "./Gambles/Wheel";
 import RPS from "./Gambles/RPS";
 import _7UD from "./Gambles/7UD";
 import _3Cards from "./Gambles/3Cards";
+import HiLoGame from "./Gambles/HiLo";
 
 const GameDetails = () => {
   const { gameName } = useParams();
@@ -19,6 +20,7 @@ const GameDetails = () => {
       {gameName === "rock-paper-scissors" && <RPS />}
       {gameName === "7-up-7-down" && <_7UD />}
       {gameName === "3-cards" && <_3Cards />}
+      {gameName === "high-low" && <HiLoGame />}
     </div>
   );
 };
